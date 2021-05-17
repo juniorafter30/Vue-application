8196ad2d3b9ac3ab2f3e192c65669592c52d6cc3
juniorafter30 <75785576+juniorafter30@users.noreply.github.com>
2021-05-17 09:50:10 +0200

    Setting up the project
    Lets start with Router, and create some basic routes for our project.

---
+++ src/router.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/coaches', component: null },
+    {
+      path: '/coaches/:id',
+      component: null,
+      children: [{ path: 'contact', component: null }] //coaches/c1/contact
+    { path: '/register', component: null },
+    { path: '/cont', component: null }
+  ]
+});
+
+export default router;
