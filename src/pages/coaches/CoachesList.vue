910052e3b922c661eff4b7f70f8e58c0824b5509
juniorafter30 <75785576+juniorafter30@users.noreply.github.com>
2021-05-18 20:19:05 +0200

    filling some routes
    moving forward

---
+++ src/pages/coaches/CoachesList.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>Filter</section>
+
+  <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register as a coach</router-link>
+    </div>
+    <ul>
+      List of Coaches
+    </ul>
+  </section>
+</template>
--- src/router.js
+++ src/router.js
@@ -1,15 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+//importing components from coaches page
+import CoachDetails from './pages/coaches/CoachDetail.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+
+//importing components from requests page
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestReceived from './pages/requests/RequestReceived.vue';
+
+//our alternative when the user will face a page that not exist
+import NotFound from './pages/NotFound.vue';
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/coaches', component: null },
+    { path: '/', redirect: '/coaches' },
+    { path: '/coaches', component: CoachesList },
     {
       path: '/coaches/:id',
-      component: null,
-      children: [{ path: 'contact', component: null }] //coaches/c1/contact
-    { path: '/register', component: null },
-    { path: '/cont', component: null }
+      component: CoachDetails,
+      children: [{ path: 'contact', component: ContactCoach }]
+    }, //coaches/c1/contact
+    { path: '/register', component: CoachRegistration },
+    { path: '/request', component: RequestReceived },
+    { path: '/:notFound(.*)', component: NotFound }
   ]
 });
 
